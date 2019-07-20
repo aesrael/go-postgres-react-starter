744009c8197e331e1496b859076a461eb072ea71
Israel Adura <israeladura@gmail.com>
2019-07-20 14:15:59 +0100

    setup postgres db

---
+++ db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+const (
+	DB_USER     = "israel"
+	DB_PASSWORD = "secret"
+	DB_NAME     = "goauth"
+)
+
+//DB instance
+var DB *sql.DB
+
+//Connect to db
+func Connect() {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
+
+	db, _ := sql.Open("postgres", dbinfo)
+	err := db.Ping()
+	if err != nil {
+		log.Fatal("Error: Could not establish a connection with the database")
+	}
+	DB = db
+	CreateUsersTable()
+}
