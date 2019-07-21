0e8b2fb6cb1fea998e08935846510cc07d5f1170
Israel Adura <israeladura@gmail.com>
2019-07-21 18:02:09 +0100

    changed folder structure

--- db/queries.go
+++
@@ -1,7 +0,0 @@
-package db
-
-const (
-	CheckUserExists = `SELECT id from users WHERE email = $1`
-	LoginQuery      = `SELECT * from users WHERE email = $1`
-	CreateUserQuery = `INSERT INTO users(id,name,password,email) VALUES (DEFAULT, $1 , $2, $3);`
-)
--- db/userschema.go
+++
@@ -1,8 +0,0 @@
-package db
-
-//CreateUsersTable //creates users table
-func CreateUsersTable() {
-	DB.Query(`
-		CREATE TABLE IF NOT EXISTS users( id serial PRIMARY KEY, name VARCHAR (100) NOT NULL, password VARCHAR (355) NOT NULL, email VARCHAR (355) UNIQUE NOT NULL, created_on TIMESTAMP NOT NULL default current_timestamp,updated_at TIMESTAMP NOT NULL default current_timestamp )`,
-	)
-}
