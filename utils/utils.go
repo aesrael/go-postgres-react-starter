2ee08c69bc670c177609ec1ad239255bfafe89e5
Israel Adura <israeladura@gmail.com>
2019-07-21 15:24:56 +0100

    better handle validation errors

--- utils/utils.go
+++ utils/utils.go
@@ -9,15 +9,13 @@ const (
 	emailRegex = `^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`
 )
 
+// ValidateUser returns a slice of string of validation errors
 func ValidateUser(user db.Register, err []string) []string {
 	emailCheck := regexp.MustCompile(emailRegex).MatchString(user.Email)
 	if emailCheck != true {
-		// err[0] = "Invalid email"
 		err = append(err, "Invalid email")
 	}
-
 	if len(user.Password) < 4 {
-		// err[1] = "Invalid password"
 		err = append(err, "Invalid password, Password should be more than 4 characters")
 	}
 	if len(user.Name) < 1 {
