306a584ef5b7f105374e81cd359ee2819c59471b
Israel Adura <israeladura@gmail.com>
2019-07-21 17:29:30 +0100

    seperate router logic

---
+++ router/router.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"go-postgres-jwt-react-starter/controller"
+	"go-postgres-jwt-react-starter/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+// SetupRouter setup routing here
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	// Middlewares
+	router.Use(middlewares.ErrorHandler)
+
+	router.GET("/ping", controller.Pong)
+	router.POST("/register", controller.Create)
+	router.POST("/login", controller.Login)
+	router.GET("/session", controller.Session)
+	return router
+}
