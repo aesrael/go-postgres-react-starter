a01be9f3524757b0f8d5529ecb2902034ead0fac
Israel Adura <israeladura@gmail.com>
2019-07-21 18:00:11 +0100

    initial react setup

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Session from "./components/Session";
+
+function App() {
+  return (
+      <Router>
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+          <Route path="/profile" component={Session} />
+      </Router>
+  );
+}
+
+export default App;
