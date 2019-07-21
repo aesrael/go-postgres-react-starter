4b139326769c86441e7f707c3da76acd4f7653ce
Israel Adura <israeladura@gmail.com>
2019-07-21 15:31:23 +0100

    changed userID to id

--- db/userschema.go
+++ db/userschema.go
@@ -4,8 +4,8 @@ package db
 func CreateUsersTable() {
 	DB.Query(`
 	CREATE TABLE IF NOT EXISTS users(
-	user_id serial PRIMARY KEY,
-	name VARCHAR (100) UNIQUE NOT NULL,
+	id serial PRIMARY KEY,
+	name VARCHAR (100) NOT NULL,
 	password VARCHAR (355) NOT NULL,
 	email VARCHAR (355) UNIQUE NOT NULL,
 	created_on TIMESTAMP NOT NULL default current_timestamp,
