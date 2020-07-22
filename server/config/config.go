11ed0bd0bdeda928b9af2f4f145062457d10fd4d
David Ilenwabor <dave11ilen@gmail.com>
2020-07-22 08:00:43 +0100

    Added user password reset functionality

--- server/config/config.go
+++ server/config/config.go
@@ -1,8 +1,8 @@
 package config
 
 const (
-	DB_USER     = "demistry"
-	DB_PASSWORD = "demistryilen"
+	DB_USER     = "davidemi"
+	DB_PASSWORD = "secret"
 	DB_NAME     = "goauth"
 	CLIENT_URL  = "http://localhost:5431"
 )
