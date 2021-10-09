a591f06fe80a18652d5bad79c14e480ae0f90073
Israel Adura <israeladura@gmail.com>
2021-10-10 00:50:43 +0100

    lint

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, } from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import Session from './pages/Session'
 import './App.css'
 
 function App() {
-    return (
-        <Router>
-            <Route exact path="/" component={Session} />
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-            <Route path="/session" component={Session} />
-        </Router>
-    )
+  return (
+    <Router>
+      <Route exact path="/" component={Session} />
+      <Route path="/register" component={Register} />
+      <Route path="/login" component={Login} />
+      <Route path="/session" component={Session} />
+    </Router>
+  )
 }
 
 export default App
--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,8 +1,8 @@
-export const createCookie = (cookieName, cookieValue, hourToExpire) => {
-    const date = new Date()
-    date.setTime(date.getTime() + hourToExpire * 60 * 60 * 1000)
-    document.cookie = `${cookieName} = ${cookieValue}; expires = ${date.toGMTString()}`
+export const createCookie = (cookieName, cookieValue, hourToExpire,) => {
+  const date = new Date()
+  date.setTime(date.getTime() + hourToExpire * 60 * 60 * 1000,)
+  document.cookie = `${cookieName} = ${cookieValue}; expires = ${date.toGMTString()}`
 }
 
-export const deleteCookie = (name) =>
-    (document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;')
+export const deleteCookie = (name,) =>
+  (document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;')
