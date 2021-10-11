b6380af9b9bdf525308b34c27e480b549eb67759
Israel Adura <israeladura@gmail.com>
2021-10-11 01:13:09 +0100

    removed unused queries

--- server/packages/db/queries.go
+++ server/packages/db/queries.go
@@ -1,10 +1,7 @@
 package db
 
 const (
-	CheckUserExists         = `SELECT true from users WHERE email = $1`
-	LoginQuery              = `SELECT * from users WHERE email = $1`
-	CreateUserQuery         = `INSERT INTO users(id, name, password, email) VALUES (DEFAULT, $1 , $2, $3);`
-	UpdateUserPasswordQuery = `UPDATE users SET password = $2 WHERE id = $1`
-	GetUserByIDQuery        = `SELECT * FROM users WHERE id = $1`
-	GetUserByEmailQuery     = `SELECT * FROM users WHERE email = $1`
+	CreateUserQuery     = `INSERT INTO users(id, name, password, email) VALUES (DEFAULT, $1 , $2, $3);`
+	GetUserByIDQuery    = `SELECT * FROM users WHERE id = $1`
+	GetUserByEmailQuery = `SELECT * FROM users WHERE email = $1`
 )
